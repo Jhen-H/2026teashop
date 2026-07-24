de92f6602b1b77463ec4f8faf92259f63cf7f8a5
Jhen-H <haha861213@gmail.com>
2026-07-24 14:39:56 +0800

    Set ORDER_ENDPOINT to the deployed script URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script Web App URL. Set this after deploying the script in apps-script/Code.gs.
-window.ORDER_ENDPOINT = "";
+window.ORDER_ENDPOINT = "https://script.google.com/macros/s/AKfycbyVWGJFtXtV5j1w0vZALGpcBIDttcttJ9HXHO-wmqyHe8W48Cu2Cfs-UGYW1854tFJQ/exec";
